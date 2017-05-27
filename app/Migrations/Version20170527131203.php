4796210a0a6f661dee649e87a0123c3947ee489d
Peter Ton <mail@peterton.nl>
2017-05-27 15:15:53 +0200

    Replaced database migration

---
+++ app/Migrations/Version20170527131203.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170527131203 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('CREATE TABLE StatusUpdate (id INT AUTO_INCREMENT NOT NULL, incidentId VARCHAR(255) NOT NULL, updateId VARCHAR(255) NOT NULL, regionSlug VARCHAR(255) NOT NULL, regionName VARCHAR(255) NOT NULL, serviceName VARCHAR(255) NOT NULL, serviceStatus VARCHAR(255) NOT NULL, author VARCHAR(255) DEFAULT NULL, content VARCHAR(255) NOT NULL, severity VARCHAR(255) NOT NULL, createdAt DATETIME NOT NULL, updateAt DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP TABLE StatusUpdate');
+    }
+}
